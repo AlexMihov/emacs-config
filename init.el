28bf4985679677bd7a044e0c0036a3274adc61ff
Alexander Mihov <mihov.alex@gmail.com>
2019-03-18 16:54:42 +0100

    [Feature] mu4e: added support for GMAIL mailbox;

--- init.el
+++ init.el
@@ -15,29 +15,30 @@
 		      dashboard
 		      doom-modeline
 		      doom-themes
-		      exec-path-from-shell
 		      evil
-                      evil-escape
-                      evil-leader
-                      evil-mc
-                      evil-numbers
-                      evil-surround
+		      exec-path-from-shell
 		      flycheck
 		      flycheck-flow
 		      flyspell
-		      git-gutter
 		      js2-mode
 		      magit
 		      markdown-mode
 		      multiple-cursors
 		      pdf-tools
 		      projectile
+		      rainbow-delimiters
 		      ranger
 		      restclient
-		      rainbow-delimiters
 		      smex
 		      visual-fill-column
-		      which-key))
+		      which-key
+		      wttrin
+                      evil-escape
+                      evil-leader
+                      evil-mc
+                      evil-numbers
+                      evil-surround
+		      ))
 (package-initialize)
 
 (dolist (p my-packages)
@@ -56,7 +57,7 @@
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
 (setq epa-pinentry-mode 'loopback)
 
-
+(setq sentence-end-double-space nil)
 ;; increase the font globally for bigger resolutions
 ;(set-face-attribute 'default nil :height 140)
 
@@ -128,6 +129,11 @@
 ;; may have their own settings.
 					;
 (load-theme 'doom-peacock t)
+
+(setq wttrin-default-cities '("Zurich"))
+(setq wttrin-default-accept-language '("Accept-Language" . "en-US"))
+
+
 
 ;;(load-theme 'webstorm t)
 
@@ -293,7 +299,80 @@
 
 ;;; END PRETRIER
 
+;;; MU4E
+(let ((default-directory "/usr/local/share/emacs/site-lisp/"))
+  (normal-top-level-add-subdirs-to-load-path))
 
+(require 'mu4e)
+
+;; use mu4e for e-mail in emacs
+(setq mail-user-agent 'mu4e-user-agent)
+
+;; default
+(setq mu4e-maildir "~/.Mail/mihov.alex@gmail.com")
+
+(setq mu4e-drafts-folder "/[Gmail].Drafts")
+(setq mu4e-sent-folder   "/[Gmail].Sent Mail")
+(setq mu4e-trash-folder  "/[Gmail].Trash")
+
+;; don't save message to Sent Messages, Gmail/IMAP takes care of this
+(setq mu4e-sent-messages-behavior 'delete)
+
+;; (See the documentation for `mu4e-sent-messages-behavior' if you have
+;; additional non-Gmail addresses and want assign them different
+;; behavior.)
+
+;; setup some handy shortcuts
+;; you can quickly switch to your Inbox -- press ``ji''
+;; then, when you want archive some messages, move them to
+;; the 'All Mail' folder by pressing ``ma''.
+
+(setq mu4e-maildir-shortcuts
+    '( ("/INBOX"               . ?i)
+       ("/[Gmail].Sent Mail"   . ?s)
+       ("/[Gmail].Trash"       . ?t)
+       ("/[Gmail].All Mail"    . ?a)))
+
+;; allow for updating mail using 'U' in the main view:
+(setq mu4e-get-mail-command "offlineimap")
+
+;; something about ourselves
+(setq
+   user-mail-address "mihov.alex@gmail.com"
+   user-full-name  "Alex Mihov"
+   mu4e-compose-signature
+    (concat
+      "Alex Mihov\n"
+      "https://mihov.ch\n"))
+
+;; sending mail -- replace USERNAME with your gmail username
+;; also, make sure the gnutls command line utils are installed
+;; package 'gnutls-bin' in Debian/Ubuntu
+
+(setq auth-sources
+      '((:source "~/.authinfo2.gpg")))
+
+(require 'smtpmail)
+(setq message-send-mail-function 'smtpmail-send-it
+   starttls-use-gnutls t
+   smtpmail-starttls-credentials '(("smtp.gmail.com" 587 nil nil))
+   smtpmail-auth-credentials
+     '(("smtp.gmail.com" 587 "mihov.alex@gmail.com" nil))
+   smtpmail-default-smtp-server "smtp.gmail.com"
+   smtpmail-smtp-server "smtp.gmail.com"
+   smtpmail-smtp-service 587)
+
+
+;; alternatively, for emacs-24 you can use:
+;;(setq message-send-mail-function 'smtpmail-send-it
+;;     smtpmail-stream-type 'starttls
+;;     smtpmail-default-smtp-server "smtp.gmail.com"
+;;     smtpmail-smtp-server "smtp.gmail.com"
+;;     smtpmail-smtp-service 587)
+
+;; don't keep message buffers around
+(setq message-kill-buffer-on-exit t)
+;;; END MU4E
 
 (put 'upcase-region 'disabled nil)
 (custom-set-variables
@@ -313,7 +392,7 @@
  '(org-agenda-files nil)
  '(package-selected-packages
    (quote
-    (git-gutter flyspell evil-surround evil-numbers evil-mc evil-leader evil-escape evil rainbow-delimiters yaml-mode csv-mode smex visual-fill-column pdf-tools multiple-cursors auto-complete ac-js2 ac-cider clojure-mode clj-refactor cider doom-themes all-the-icons doom-modeline projectile ranger dashboard markdown-mode flyspell-correct flycheck-flow flycheck exec-path-from-shell which-key magit restclient))))
+    (wttrin yahoo-weather all-the-icons-dired git-gutter flyspell evil-surround evil-numbers evil-mc evil-leader evil-escape evil rainbow-delimiters yaml-mode csv-mode smex visual-fill-column pdf-tools multiple-cursors auto-complete ac-js2 ac-cider clojure-mode clj-refactor cider doom-themes all-the-icons doom-modeline projectile ranger dashboard markdown-mode flyspell-correct flycheck-flow flycheck exec-path-from-shell which-key magit restclient))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
